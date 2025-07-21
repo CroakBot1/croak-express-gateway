ef034bb1e070d51979844260b05f5b4b4a30dda2
CroakBot1 <apploverss1@gmail.com>
2025-07-21 08:28:56 +0800

    Delete backend-trader directory

--- backend-trader/core/priceFetcher.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-async function fetchPrice() {
-  const res = await axios.get('https://api.bybit.com/v2/public/tickers?symbol=ETHUSDT');
-  return parseFloat(res.data.result[0].last_price);
-}
-
-module.exports = { fetchPrice };
