a1557b2d0c6c8235231b9408f955ea98625cc47c
CroakBot1 <apploverss1@gmail.com>
2025-08-01 21:54:00 -0700

    Delete project-root directory

--- project-root/src/backend-trader/trader.js
+++
@@ -1,8 +0,0 @@
-async function executeTrade(direction = "BUY") {
-  // Placeholder for real Uniswap trade logic
-  console.log(`💥 Executing ${direction} on Uniswap...`);
-  // TODO: Integrate web3 + private key signer + swap execution
-}
-
-module.exports = { executeTrade };
-
